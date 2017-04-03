42847a5a64e905a35f17163a5b36d8188eaeac4c
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-04-03 21:10:49 +0200

    routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,2 +1,13 @@
 <?php
 
+/*
+|----------------------------------
+| GROUPS
+|----------------------------------
+*/
+Route::get('api/v1/countries/{lang?}',                             ['as' => 'country',                         'uses' => 'Syscover\Admin\Controllers\CountryController@index']);
+Route::get('api/v1/countries/{id}/{lang}',                         ['as' => 'showCountry',                     'uses' => 'Syscover\Admin\Controllers\CountryController@show']);
+Route::post('api/v1/countries',                                    ['as' => 'storeCountry',                    'uses' => 'Syscover\Admin\Controllers\CountryController@store']);
+Route::put('api/v1/countries/{id}/{lang}',                         ['as' => 'updateAction',                    'uses' => 'Syscover\Admin\Controllers\CountryController@update']);
+Route::delete('api/v1/countries/{id}/{lang?}',                     ['as' => 'destroyAction',                   'uses' => 'Syscover\Admin\Controllers\CountryController@destroy']);
+
