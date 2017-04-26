46a5121b2472f2407805fb436ec9b5a5da296a60
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-04-26 21:23:49 +0200

    refactor code

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,10 +5,10 @@
 | GROUPS
 |----------------------------------
 */
-Route::get('api/v1/crm/groups',                                       ['as' => 'group',                          'uses' => 'Syscover\Crm\Controllers\GroupController@index']);
-Route::get('api/v1/crm/groups/{id}',                                  ['as' => 'showGroup',                      'uses' => 'Syscover\Crm\Controllers\GroupController@show']);
-Route::post('api/v1/crm/groups',                                      ['as' => 'storeGroup',                     'uses' => 'Syscover\Crm\Controllers\GroupController@store']);
-Route::post('api/v1/crm/groups/search',                               ['as' => 'searchGroup',                    'uses' => 'Syscover\Crm\Controllers\GroupController@search']);
-Route::put('api/v1/crm/groups/{id}',                                  ['as' => 'updateGroup',                    'uses' => 'Syscover\Crm\Controllers\GroupController@update']);
-Route::delete('api/v1/crm/groups/{id}',                               ['as' => 'destroyGroup',                   'uses' => 'Syscover\Crm\Controllers\GroupController@destroy']);
+Route::get('api/v1/crm/group',                                       ['as' => 'group',                          'uses' => 'Syscover\Crm\Controllers\GroupController@index']);
+Route::get('api/v1/crm/group/{id}',                                  ['as' => 'showGroup',                      'uses' => 'Syscover\Crm\Controllers\GroupController@show']);
+Route::post('api/v1/crm/group',                                      ['as' => 'storeGroup',                     'uses' => 'Syscover\Crm\Controllers\GroupController@store']);
+Route::post('api/v1/crm/group/search',                               ['as' => 'searchGroup',                    'uses' => 'Syscover\Crm\Controllers\GroupController@search']);
+Route::put('api/v1/crm/group/{id}',                                  ['as' => 'updateGroup',                    'uses' => 'Syscover\Crm\Controllers\GroupController@update']);
+Route::delete('api/v1/crm/group/{id}',                               ['as' => 'destroyGroup',                   'uses' => 'Syscover\Crm\Controllers\GroupController@destroy']);
 
