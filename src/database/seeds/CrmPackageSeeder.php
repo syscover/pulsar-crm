35b57472c5a6cc3545686d6ed248f7b0e8ae5ae3
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-08-18 11:08:09 +0200

    updates database

--- src/database/seeds/CrmPackageSeeder.php
+++ src/database/seeds/CrmPackageSeeder.php
@@ -8,7 +8,7 @@ class CrmPackageSeeder extends Seeder
     public function run()
     {
         Package::insert([
-            ['id' => '9', 'name' => 'CRM Package', 'root' => 'crm', 'sort' => 9, 'active' => false]
+            ['id' => 90, 'name' => 'CRM Package', 'root' => 'crm', 'sort' => 90, 'active' => true]
         ]);
     }
 }
