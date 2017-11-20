6012e3668186a2c95f93beed40ae8701752f2d46
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-11-20 20:55:57 +0100

    add object_id to seed

--- src/database/seeds/CrmResourceSeeder.php
+++ src/database/seeds/CrmResourceSeeder.php
@@ -8,11 +8,11 @@ class CrmResourceSeeder extends Seeder {
     public function run()
     {
         Resource::insert([
-            ['id' => 'crm',             'name' => 'CRM Package',    'package_id' => 90],
-            ['id' => 'crm-address',     'name' => 'Address',        'package_id' => 90],
-            ['id' => 'crm-customer',    'name' => 'Customers',      'package_id' => 90],
-            ['id' => 'crm-group',       'name' => 'Groups',         'package_id' => 90],
-            ['id' => 'crm-type',        'name' => 'Types',          'package_id' => 90],
+            ['object_id' => 'crm',             'name' => 'CRM Package',    'package_id' => 90],
+            ['object_id' => 'crm-address',     'name' => 'Address',        'package_id' => 90],
+            ['object_id' => 'crm-customer',    'name' => 'Customers',      'package_id' => 90],
+            ['object_id' => 'crm-group',       'name' => 'Groups',         'package_id' => 90],
+            ['object_id' => 'crm-type',        'name' => 'Types',          'package_id' => 90],
         ]);
     }
 }
