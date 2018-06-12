03d6e0c9ac52c48dcee428ea2c19782de4c2b9d3
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-06-12 19:31:49 +0200

    minor fix

--- src/database/migrations/0090_01_01_000010_crm_create_table_customer_group.php
+++ src/database/migrations/0090_01_01_000010_crm_create_table_customer_group.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CrmCreateTableGroup extends Migration {
+class CrmCreateTableCustomerGroup extends Migration {
 
     /**
      * Run the migrations.
