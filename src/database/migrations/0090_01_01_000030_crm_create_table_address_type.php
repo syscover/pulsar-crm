b68477ad370fe658c292f33753a8384f178be675
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-06-12 19:39:29 +0200

    minor fix

--- src/database/migrations/0090_01_01_000030_crm_create_table_address_type.php
+++ src/database/migrations/0090_01_01_000030_crm_create_table_address_type.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CrmCreateTableType extends Migration {
+class CrmCreateTableAddressType extends Migration {
 
     /**
      * Run the migrations.
