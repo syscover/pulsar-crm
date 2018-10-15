3b482dc913555a66f024b49283d63cbf7892c4c6
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-10-15 10:39:38 +0200

    minor fix

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,27 +2,8 @@
 
 /*
 |----------------------------------
-| GROUPS
-|----------------------------------
-*/
-//Route::get('api/v1/crm/group',                  'Syscover\Crm\Controllers\GroupController@index')->name('api.crm_group.index');
-//Route::get('api/v1/crm/group/{id}',             'Syscover\Crm\Controllers\GroupController@show')->name('api.crm_group.show');
-//Route::post('api/v1/crm/group',                 'Syscover\Crm\Controllers\GroupController@store')->name('api.crm_group.store');
-//Route::post('api/v1/crm/group/search',          'Syscover\Crm\Controllers\GroupController@search')->name('api.crm_group.search');
-//Route::put('api/v1/crm/group/{id}',             'Syscover\Crm\Controllers\GroupController@update')->name('api.crm_group.update');
-//Route::delete('api/v1/crm/group/{id}',          'Syscover\Crm\Controllers\GroupController@destroy')->name('api.crm_group.destroy');
-
-/*
-|----------------------------------
 | ADDRESS
 |----------------------------------
 */
 Route::put('api/v1/crm/address/{id}',           'Syscover\Crm\Controllers\AddressController@update')->name('pulsar.crm.address.update');
-
-/*
-|----------------------------------
-| CUSTOMER
-|----------------------------------
-*/
-Route::post('api/v1/crm/customer/has_user',     'Syscover\Crm\Controllers\CustomerController@hasUser')->name('pulsar.crm.customer.has_user')->middleware('web');
 
