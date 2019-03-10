6c0d81016fd55f80c939ded24dbea6db1fb731ba
Carlos Rodríguez Palacín <cpalacin@digitalh2.com>
2019-03-10 15:07:04 +0100

    set resources in menu

--- src/database/seeds/CrmResourceSeeder.php
+++ src/database/seeds/CrmResourceSeeder.php
@@ -8,11 +8,11 @@ class CrmResourceSeeder extends Seeder {
     public function run()
     {
         Resource::insert([
-            // ['id' => 'crm',             'name' => 'CRM Package',    'package_id' => 90],
-            // ['id' => 'crm-address',     'name' => 'Address',        'package_id' => 90],
-            ['id' => 'crm-customer',    'name' => 'Customers',      'package_id' => 90],
-            ['id' => 'crm-group',       'name' => 'Groups',         'package_id' => 90],
-            ['id' => 'crm-type',        'name' => 'Types',          'package_id' => 90],
+            ['id' => 'crm',                 'name' => 'CRM Package',    'package_id' => 90],
+            ['id' => 'crm-address',         'name' => 'Address',        'package_id' => 90],
+            ['id' => 'crm-customer',        'name' => 'Customers',      'package_id' => 90],
+            ['id' => 'crm-group',           'name' => 'Groups',         'package_id' => 90],
+            ['id' => 'crm-address-type',    'name' => 'Address Types',  'package_id' => 90],
         ]);
     }
 }
