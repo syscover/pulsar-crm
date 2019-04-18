e38246eced15b490b08cc446e1a8b7b38d5f5c13
Carlos Rodríguez Palacín <cpalacin@digitalh2.com>
2019-04-19 00:24:14 +0200

    change translatable by langable

--- src/Syscover/Crm/Models/Customer.php
+++ src/Syscover/Crm/Models/Customer.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Syscover\Crm\Notifications\ResetPassword as ResetPasswordNotification;
-use Syscover\Admin\Traits\Translatable;
+use Syscover\Admin\Traits\Langable;
 use Syscover\Core\Models\CoreModel;
 use Syscover\Crm\Events\ResetLinkEmailSent;
 
@@ -26,7 +26,7 @@ class Customer extends CoreModel implements
 {
     use Authenticatable, Authorizable, CanResetPassword;
     use Notifiable;
-    use Translatable;
+    use Langable;
 
 	protected $table        = 'crm_customer';
     protected $fillable     = ['id', 'lang_id', 'group_id', 'date', 'company', 'tin', 'gender_id', 'treatment_id', 'state_id', 'name', 'surname', 'avatar', 'birth_date', 'email', 'phone', 'mobile', 'user', 'password', 'active', 'confirmed', 'country_id', 'territorial_area_1_id', 'territorial_area_2_id', 'territorial_area_3_id', 'zip', 'locality', 'address', 'latitude', 'longitude', 'field_group_id', 'data'];
